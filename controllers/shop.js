6feacb232d74483e01f6fc6fe0ba98bf51ec4de9
Shirley Eunice M <shirleyjustin20@gmail.com>
2026-03-18 00:59:38 +0530

    View created for Products, Admin, Controller basic structure initialized

---
+++ controllers/shop.js
@@ -0,0 +1,52 @@
+const Product = require("../models/product");
+// exports.getAddProduct = (req, res, next) => {
+//   res.render('admin/add-product', {
+//     pageTitle: 'Add Product',
+//     path: '/admin/add-product',
+//     formsCSS: true,
+//     productCSS: true,
+//     activeAddProduct: true
+//   });
+// }
+
+
+// exports.postAddProduct = (req, res, next) => {
+//   const product = new Product(req.body.title);
+//   product.save();
+//   res.redirect('/');
+// }
+
+exports.getProduct = (req, res, next) => {
+  Product.fetchAll((products)=>{
+    res.render('shop/product-list', {
+    prods: products,
+    pageTitle: 'All Products',
+    path: '/products',
+  });
+  });
+  
+}
+
+exports.getIndex = (req, res, next)=>{
+    Product.fetchAll((products) => {
+      res.render("shop/index", {
+        prods: products,
+        pageTitle: "Shop",
+        path: "/",
+      });
+    });
+}
+
+exports.getCart = (req, res, next) =>{
+    res.render("shop/cart", {
+        path:"/cart",
+        pageTitle: "Your Cart"
+    })
+}
+
+exports.getCheckout = (req, res, next) =>{
+    res.render("shop/checkout", {
+        path:"/checkout",
+        pageTitle: "Checkout"
+    })
+}
